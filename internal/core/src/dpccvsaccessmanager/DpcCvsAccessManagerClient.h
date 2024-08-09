eeefb7b2e3390da6d0e096101df487857c09e4c4
Sriram Ganesh <sriram.ganesh@salesforce.com>
2024-08-09 14:21:02 -0700

    @W-16450790 Access manager gRPC client

---
+++ internal/core/src/dpccvsaccessmanager/DpcCvsAccessManagerClient.h
@@ -0,0 +1,30 @@
+#ifndef DPC_CVS_ACCESS_MANAGER_CLIENT_H
+#define DPC_CVS_ACCESS_MANAGER_CLIENT_H
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <grpcpp/grpcpp.h>
+#include "pb/dpc_cvs_access_manager.pb.h"
+#include "pb/dpc_cvs_access_manager.grpc.pb.h"
+
+namespace milvus::dpccvsaccessmanager {
+
+class DpcCvsAccessManagerClient {
+public:
+	DpcCvsAccessManagerClient();
+    salesforce::cdp::dpccvsaccessmanager::v1::GetCredentialsResponse GetCredentials(
+        const std::string& collection_id,
+        const std::string& instance_name,
+        const std::string& bucket_name,
+        bool write_access);
+
+private:
+    std::unique_ptr<salesforce::cdp::dpccvsaccessmanager::v1::DpcCvsAccessManager::Stub> stub_;
+	static std::mutex stub_mutex_;
+    std::shared_ptr<grpc::Channel> channel_;
+};
+
+}
+#endif
